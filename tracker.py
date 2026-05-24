c504525f06ba88e19012683f3d30ce930ad770f9
unknown <nairn036@gmail.com>
2026-05-24 22:01:37 +0100

    Intial commit: Toxic chat filter working

---
+++ tracker.py
@@ -0,0 +1,8 @@
+banned_words_lol = ["naughty naughty word_1","lololol!"]
+
+using_person_message = input("type your message here!")
+
+
+
+if using_person_message in banned_words_lol:
+    print("FILTERED!")
